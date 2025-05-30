f9ccb668b41a602f1875773d352f0393d556d716
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-30 15:15:37 +0200

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={""} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/pages/homepage.jsx
@@ -0,0 +1,4 @@
+export default const homePage =() => {
+    return <>
+    <h1>homepage</h1></>
+}
